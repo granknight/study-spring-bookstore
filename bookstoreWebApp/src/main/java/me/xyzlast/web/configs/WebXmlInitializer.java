7d13edeb8ae6b31516bd972330a0d0be0158b464
ykyoon <xyzlast@gmail.com>
2014-03-10 09:44:27 +0900

    implement webxmlInitializer

--- bookstoreWebApp/src/main/java/me/xyzlast/web/configs/WebXmlInitializer.java
+++ bookstoreWebApp/src/main/java/me/xyzlast/web/configs/WebXmlInitializer.java
@@ -1,23 +1,34 @@
-//package me.xyzlast.web.configs;
-//
-//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-//
-///**
-// * Created by ykyoon on 3/7/14.
-// */
-//public class WebXmlInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-//    @Override
-//    protected Class<?>[] getRootConfigClasses() {
-//        return new Class<?>[] { EmptyDomainConfiguration.class };
-//    }
-//
-//    @Override
-//    protected Class<?>[] getServletConfigClasses() {
-//        return new Class<?>[] { ControllerConfiguration.class };
-//    }
-//
-//    @Override
-//    protected String[] getServletMappings() {
-//        return new String[] { "/" };
-//    }
-//}
+package me.xyzlast.web.configs;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+* Created by ykyoon on 3/7/14.
+* no-web.xml configuration
+*/
+public class WebXmlInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { EmptyDomainConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { ControllerConfiguration.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        return new Filter[] { encodingFilter };
+    }
+}
